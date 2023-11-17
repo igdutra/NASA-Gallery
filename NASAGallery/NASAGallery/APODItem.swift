f8f68a63fb3543d74b4c591fa99f2f327169f799
Ivo Dutra <ivogdutra@gmail.com>
2023-11-17 17:28:27 -0300

    Create the APODItem Model

--- NASAGallery/NASAGallery/APODItem.swift
+++ NASAGallery/NASAGallery/APODItem.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct APODItem {
+    let title: String
+    let url: URL
+    let date: String
+    let explanation: String
+    let mediaType: String
+    
+    // Optional fields
+    let copyright: String?
+    let hdurl: URL?
+    let thumbnailUrl: URL?
+
+    enum CodingKeys: String, CodingKey {
+        case date, explanation, title, url, hdurl
+        case mediaType = "media_type"
+        case thumbnailUrl = "thumbnail_url"
+        case copyright
+    }
+}
