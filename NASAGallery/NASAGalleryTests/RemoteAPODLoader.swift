2758cf39abfeb7e053673f323902791960f6ba67
Ivo Dutra <ivogdutra@gmail.com>
2023-11-20 20:41:53 -0300

    Request data from URL on load

--- NASAGallery/NASAGalleryTests/RemoteAPODLoader.swift
+++ NASAGallery/NASAGalleryTests/RemoteAPODLoader.swift
@@ -8,7 +8,17 @@
 import XCTest
 
 class RemoteAPODLoader {
+    let client: HTTPClient
+    let url: URL
     
+    init(url: URL = URL(string: "a-url.com")!, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    func load() {
+        client.get(from: url)
+    }
 }
 
 protocol HTTPClient {
@@ -19,7 +29,7 @@ class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
     
     func get(from url: URL) {
-        
+        requestedURL = url
     }
 }
 
@@ -27,9 +37,19 @@ class HTTPClientSpy: HTTPClient {
 final class RemoteAPODLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let sut = RemoteAPODLoader()
         let client = HTTPClientSpy()
+        let sut = RemoteAPODLoader(client: client)
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL() {
+        let url = URL(string: "b-url.com")!
+        let client = HTTPClientSpy()
+        let sut = RemoteAPODLoader(url: url, client: client)
+        
+        sut.load()
+        
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
