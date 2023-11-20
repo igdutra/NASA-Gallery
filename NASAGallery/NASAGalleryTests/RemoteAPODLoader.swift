a98887f0d67efac283e18d5df5eed00489fdc556
Ivo Dutra <ivogdutra@gmail.com>
2023-11-20 20:31:33 -0300

    Add empty test file

---
+++ NASAGallery/NASAGalleryTests/RemoteAPODLoader.swift
@@ -0,0 +1,15 @@
+//
+//  RemoteAPODLoader.swift
+//  NASAGalleryTests
+//
+//  Created by Ivo on 20/11/23.
+//
+
+import XCTest
+
+final class RemoteAPODLoader: XCTestCase {
+
+    func test() {
+        XCTFail("Initial failing test.")
+    }
+}
