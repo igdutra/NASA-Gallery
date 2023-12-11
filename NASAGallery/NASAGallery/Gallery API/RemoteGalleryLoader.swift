2d0faebf09ad5a89b93dbb27191c0e7d596f37fe
Ivo Dutra <ivogdutra@gmail.com>
2023-12-11 17:52:55 -0300

    Refactor `RemoteGalleryLoader` to use `RemoteGalleryLoaderMapper` with Root struct without breaking the tests

--- NASAGallery/NASAGallery/Gallery API/RemoteGalleryLoader.swift
+++ NASAGallery/NASAGallery/Gallery API/RemoteGalleryLoader.swift
@@ -25,16 +25,10 @@ public class RemoteGalleryLoader {
             throw Error.connectivity
         }
         
-        
-        if response.statusCode == 200 {
-            do {
-                let items = try JSONDecoder().decode([GalleryItem].self, from: data)
-                return items
-            } catch {
-                print(error)
-                throw Error.invalidData
-            }
-        } else {
+        do {
+            let items = try RemoteGalleryLoaderMapper.map(data, response: response)
+            return items
+        } catch {
             throw Error.invalidData
         }
     }
@@ -44,5 +38,43 @@ public class RemoteGalleryLoader {
     public enum Error: Swift.Error {
         case connectivity
         case invalidData
+    }
+}
+
+enum RemoteGalleryLoaderMapper {
+    
+    private static let OK_200: Int = 200
+    
+    private struct Root: Decodable {
+        let title: String
+        let url: URL
+        let date: String
+        let explanation: String
+        let mediaType: String
+        let copyright: String?
+        let hdurl: URL?
+        let thumbnailUrl: URL?
+        
+        enum CodingKeys: String, CodingKey {
+            case date, explanation, title, url, hdurl
+            case mediaType = "media_type"
+            case thumbnailUrl = "thumbnail_url"
+            case copyright
+        }
+        
+        var galleryItem: GalleryItem {
+            GalleryItem(title: title, url: url, date: date, explanation: explanation, mediaType: mediaType, copyright: copyright, hdurl: hdurl, thumbnailUrl: thumbnailUrl)
+        }
+    }
+
+    static func map(_ data: Data, response: HTTPURLResponse) throws -> [GalleryItem] {
+        guard response.statusCode == OK_200 else {
+            throw RemoteGalleryLoader.Error.invalidData
+        }
+        
+        let items = try JSONDecoder().decode([Root].self, from: data)
+        let galleryItems = items.map { $0.galleryItem }
+        
+        return galleryItems
     }
 }
