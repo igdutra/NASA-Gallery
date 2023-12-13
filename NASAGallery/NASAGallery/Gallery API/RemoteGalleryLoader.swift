08a143340a8f4da722724ef2b9ab9dbc1b7dfaa8
Ivo Dutra <ivogdutra@gmail.com>
2023-12-13 13:34:27 -0300

    move `RemoteGalleryMapper` to it's own file

--- NASAGallery/NASAGallery/Gallery API/RemoteGalleryLoader.swift
+++ NASAGallery/NASAGallery/Gallery API/RemoteGalleryLoader.swift
@@ -22,7 +22,7 @@ public class RemoteGalleryLoader {
         }
         
         do {
-            let items = try RemoteGalleryLoaderMapper.map(data, response: response)
+            let items = try RemoteGalleryMapper.map(data, response: response)
             return items
         } catch {
             throw Error.invalidData
@@ -34,43 +34,5 @@ public class RemoteGalleryLoader {
     public enum Error: Swift.Error {
         case connectivity
         case invalidData
-    }
-}
-
-enum RemoteGalleryLoaderMapper {
-    
-    private static let OK_200: Int = 200
-    
-    private struct Root: Decodable {
-        let title: String
-        let url: URL
-        let date: String
-        let explanation: String
-        let mediaType: String
-        let copyright: String?
-        let hdurl: URL?
-        let thumbnailUrl: URL?
-        
-        enum CodingKeys: String, CodingKey {
-            case date, explanation, title, url, hdurl
-            case mediaType = "media_type"
-            case thumbnailUrl = "thumbnail_url"
-            case copyright
-        }
-        
-        var galleryItem: GalleryItem {
-            GalleryItem(title: title, url: url, date: date, explanation: explanation, mediaType: mediaType, copyright: copyright, hdurl: hdurl, thumbnailUrl: thumbnailUrl)
-        }
-    }
-
-    static func map(_ data: Data, response: HTTPURLResponse) throws -> [GalleryItem] {
-        guard response.statusCode == OK_200 else {
-            throw RemoteGalleryLoader.Error.invalidData
-        }
-        
-        let items = try JSONDecoder().decode([Root].self, from: data)
-        let galleryItems = items.map { $0.galleryItem }
-        
-        return galleryItems
     }
 }
---
+++ NASAGallery/NASAGallery/Gallery API/RemoteGalleryMapper.swift
@@ -0,0 +1,46 @@
+//
+//  RemoteGalleryMapper.swift
+//  NASAGallery
+//
+//  Created by Ivo on 13/12/23.
+//
+
+import Foundation
+
+enum RemoteGalleryMapper {
+    
+    private static let OK_200: Int = 200
+    
+    private struct Root: Decodable {
+        let title: String
+        let url: URL
+        let date: String
+        let explanation: String
+        let mediaType: String
+        let copyright: String?
+        let hdurl: URL?
+        let thumbnailUrl: URL?
+        
+        enum CodingKeys: String, CodingKey {
+            case date, explanation, title, url, hdurl
+            case mediaType = "media_type"
+            case thumbnailUrl = "thumbnail_url"
+            case copyright
+        }
+        
+        var galleryItem: GalleryItem {
+            GalleryItem(title: title, url: url, date: date, explanation: explanation, mediaType: mediaType, copyright: copyright, hdurl: hdurl, thumbnailUrl: thumbnailUrl)
+        }
+    }
+
+    public static func map(_ data: Data, response: HTTPURLResponse) throws -> [GalleryItem] {
+        guard response.statusCode == OK_200 else {
+            throw RemoteGalleryLoader.Error.invalidData
+        }
+        
+        let items = try JSONDecoder().decode([Root].self, from: data)
+        let galleryItems = items.map { $0.galleryItem }
+        
+        return galleryItems
+    }
+}
