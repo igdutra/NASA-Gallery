7c5786325f4b172b4991ffeaec28d1155fb0dcd1
Ivo Dutra <ivogdutra@gmail.com>
2023-12-18 20:55:20 -0300

    Refactor URLSession mocking strategy with URLProtocol stubs to remove test coupling with the URLSession types

--- NASAGallery/NASAGalleryTests/Gallery API/URLSessionHTTPClientTests.swift
+++ NASAGallery/NASAGalleryTests/Gallery API/URLSessionHTTPClientTests.swift
@@ -21,9 +21,9 @@ import XCTest
 */
 final class URLSessionHTTPClient {
     // Note how must be the protocol in production code and not the real URLSession
-    let session: URLSessionProtocol
+    let session: URLSession
     
-    init(session: URLSessionProtocol) {
+    init(session: URLSession = .shared) {
         self.session = session
     }
     
@@ -34,32 +34,111 @@ final class URLSessionHTTPClient {
 
 final class URLSessionHTTPClientTests: XCTestCase {
     
-    func test_getData_firesSessionDataFromURL() async {
-        let url = anyURL()
-        let sessionSpy = URLSessionSpy()
-        sessionSpy.stub(url: url, error: AnyError())
-        let sut = URLSessionHTTPClient(session: sessionSpy)
-        
-        try? await sut.getData(from: url)
-        
-        XCTAssertEqual(sessionSpy.receivedMessages, [.data(url)])
-    }
+//    func test_getData_firesSessionDataFromURL() async {
+//        let url = anyURL()
+//        let sessionSpy = URLSessionSpy()
+//        sessionSpy.stub(url: url, error: AnyError())
+//        let sut = URLSessionHTTPClient(session: sessionSpy)
+//        
+//        try? await sut.getData(from: url)
+//        
+//        XCTAssertEqual(sessionSpy.receivedMessages, [.data(url)])
+//    }
     
-    func test_getFromURL_failsOnRequestError() async {
+//    func test_getFromURL_failsOnRequestError() async {
+//        let url = anyURL()
+//        let expectedError = AnyError(message: "Expected Error")
+//        let sessionSpy = URLSessionSpy()
+//        sessionSpy.stub(url: url, error: expectedError)
+//        let sut = URLSessionHTTPClient(session: sessionSpy)
+//
+//        do {
+//            try await sut.getData(from: url)
+//            XCTFail("Expected Error but returned successfully instead")
+//        } catch {
+//            XCTAssertEqual(error as? AnyError, expectedError)
+//        }
+//    }
+    
+    func test_getFromURL_failsOnRequestError2() async {
         let url = anyURL()
-        let expectedError = AnyError(message: "Expected Error")
-        let sessionSpy = URLSessionSpy()
-        sessionSpy.stub(url: url, error: expectedError)
-        let sut = URLSessionHTTPClient(session: sessionSpy)
+        // Return from URLprotocol stub is AnyError wrapped into NSError. casting error as? AnyError Fails.
+        let expectedError = NSError(domain: "any error", code: 13)
+        URLProtocolStub.startInterceptingRequests()
+        
+        URLProtocolStub.stub(url: url, data: nil, response: nil, error: expectedError)
+        let sut = URLSessionHTTPClient(session: .shared)
 
         do {
             try await sut.getData(from: url)
             XCTFail("Expected Error but returned successfully instead")
-        } catch {
-            XCTAssertEqual(error as? AnyError, expectedError)
+        } catch let nsError as NSError {
+            XCTAssertEqual(nsError.domain, expectedError.domain)
         }
+        
+        URLProtocolStub.stopInterceptingRequests()
     }
 }
+
+// MARK: - URLProtocolStub
+
+private extension URLSessionHTTPClientTests {
+    private class URLProtocolStub: URLProtocol {
+        // MARK: Properties and Helpers
+        
+        private static var stubs: [URL: Stub] = .init()
+        
+        private struct Stub {
+            let data: Data?
+            let response: URLResponse?
+            let error: Error?
+        }
+        
+        static func stub(url: URL, data: Data?, response: URLResponse?, error: Error?) {
+            stubs[url] = Stub(data: data, response: response, error: error)
+        }
+        
+        static func startInterceptingRequests() {
+            URLProtocol.registerClass(URLProtocolStub.self)
+        }
+        
+        static func stopInterceptingRequests() {
+            URLProtocol.unregisterClass(URLProtocolStub.self)
+            stubs = [:]
+        }
+        
+        // MARK: - URLProtocol
+        override class func canInit(with: URLRequest) -> Bool { true }
+        
+        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+            return request
+        }
+        
+        override func startLoading() {
+            guard let url = request.url, let stub = URLProtocolStub.stubs[url] else { XCTFail("Should mock"); return }
+            
+            
+            
+            if let data = stub.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = stub.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+            
+            if let error = stub.error {
+                client?.urlProtocol(self, didFailWithError: error)
+            }
+            
+            client?.urlProtocolDidFinishLoading(self)
+        }
+        
+        override func stopLoading() { }
+    }
+}
+
+// MARK: - Alternative Approach: Protocol-based mocking
 
 protocol URLSessionProtocol {
     func data(from url: URL) async throws -> (Data, URLResponse)
@@ -67,9 +146,7 @@ protocol URLSessionProtocol {
 
 extension URLSession: URLSessionProtocol { }
 
-// MARK: - Spy
-
-final class URLSessionSpy: URLSessionProtocol {
+private final class URLSessionSpy: URLSessionProtocol {
     // MARK: Messages
     enum ReceivedMessage: Equatable {
         case data(URL)
