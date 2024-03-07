d66497539a71fcf3bda3b718b733bcead1deb619
Ivo Dutra <ivogdutra@gmail.com>
2024-03-06 21:15:49 -0300

    Assert that save command requests precious cache deletion

--- NASAGallery/NASAGalleryTests/Gallery Cache/CacheGalleryUseCaseTests.swift
+++ NASAGallery/NASAGalleryTests/Gallery Cache/CacheGalleryUseCaseTests.swift
@@ -13,11 +13,16 @@ final class LocalGalleryLoader {
     init(store: GalleryStore) {
         self.store = store
     }
+    
+    func save() {
+        store.deleteCachedGallery()
+    }
 }
 
 final class GalleryStore {
     enum ReceivedMessage: Equatable {
         case delete
+        case insert
     }
     
     private(set) var receivedMessages = [ReceivedMessage]()
@@ -25,6 +30,7 @@ final class GalleryStore {
     func deleteCachedGallery() {
         receivedMessages.append(.delete)
     }
+
 }
 
 final class CacheGalleryUseCaseTests: XCTestCase {
@@ -34,5 +40,14 @@ final class CacheGalleryUseCaseTests: XCTestCase {
         let sut = LocalGalleryLoader(store: store)
         
         XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = GalleryStore()
+        let sut = LocalGalleryLoader(store: store)
+        
+        sut.save()
+        
+        XCTAssertEqual(store.receivedMessages, [.delete])
     }
 }
