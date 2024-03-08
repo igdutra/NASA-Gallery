55c84794da0670111a0830b9318ddc7bda7ddcc4
Ivo Dutra <ivogdutra@gmail.com>
2024-03-08 10:57:47 -0300

    Assert that save command will fail on insertionError

--- NASAGallery/NASAGalleryTests/Gallery Cache/CacheGalleryUseCaseTests.swift
+++ NASAGallery/NASAGalleryTests/Gallery Cache/CacheGalleryUseCaseTests.swift
@@ -18,7 +18,7 @@ final class LocalGalleryLoader {
     // TODO: Verify about injecting closure as date
     func save(gallery: [GalleryItem], timestamp: Date) throws {
         try store.deleteCachedGallery()
-        store.insertCache(gallery: gallery, timestamp: timestamp)
+        try store.insertCache(gallery: gallery, timestamp: timestamp)
     }
 }
 
@@ -31,13 +31,14 @@ final class GalleryStore {
     private(set) var receivedMessages = [ReceivedMessage]()
     
     private struct Stub {
-        let error: Error?
+        let deletionError: Error?
+        let insertionError: Error?
     }
     
     private var stub: Stub?
     
-    func stub(error: Error?) {
-        stub = Stub(error: error)
+    func stub(deletionError: Error?, insertionError: Error?) {
+        stub = Stub(deletionError: deletionError, insertionError: insertionError)
     }
     
     // MARK: - Methods
@@ -45,13 +46,17 @@ final class GalleryStore {
     func deleteCachedGallery() throws {
         receivedMessages.append(.delete)
         
-        if let error = stub?.error {
+        if let error = stub?.deletionError {
             throw error
         }
     }
     
-    func insertCache(gallery: [GalleryItem], timestamp: Date) {
+    func insertCache(gallery: [GalleryItem], timestamp: Date) throws {
         receivedMessages.append(.insert(gallery, timestamp))
+        
+        if let error = stub?.insertionError {
+            throw error
+        }
     }
 }
 
@@ -75,7 +80,7 @@ final class CacheGalleryUseCaseTests: XCTestCase {
     func test_save_onDeletionError_shouldNotInsertCache() {
         let (sut, store) = makeSUT()
         let deletionError = AnyError(message: "Deletion Error")
-        store.stub(error: deletionError)
+        store.stub(deletionError: deletionError, insertionError: nil)
         
         try? sut.save(gallery: [], timestamp: Date())
         
@@ -86,8 +91,7 @@ final class CacheGalleryUseCaseTests: XCTestCase {
         let (sut, store) = makeSUT()
         let gallery: [GalleryItem] = makeItems().model
         let timestamp = Date()
-        
-        store.stub(error: nil) // Making test explicit that deletion error is nil
+        store.stub(deletionError: nil, insertionError: nil) // Making test explicit that deletion error is nil
         
         try? sut.save(gallery: gallery, timestamp: timestamp)
         
@@ -97,12 +101,26 @@ final class CacheGalleryUseCaseTests: XCTestCase {
     func test_save_onDeletionError_fails() {
         let (sut, store) = makeSUT()
         let deletionError = AnyError(message: "Deletion Error")
-        store.stub(error: deletionError)
+        store.stub(deletionError: deletionError, insertionError: nil)
         
         do {
             try sut.save(gallery: [], timestamp: Date())
         } catch let error as AnyError {
             XCTAssertEqual(error, deletionError)
+        } catch {
+            XCTFail("Expected error to be AnyError, got \(error) instead")
+        }
+    }
+    
+    func test_save_onInsertionError_fails() {
+        let (sut, store) = makeSUT()
+        let insertionError = AnyError(message: "Insertion Error")
+        store.stub(deletionError: nil, insertionError: insertionError)
+        
+        do {
+            try sut.save(gallery: [], timestamp: Date())
+        } catch let error as AnyError {
+            XCTAssertEqual(error, insertionError)
         } catch {
             XCTFail("Expected error to be AnyError, got \(error) instead")
         }
