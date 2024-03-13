6837768bbc368ece254e3f339eae86320db7d634
Ivo Dutra <ivogdutra@gmail.com>
2024-03-13 20:03:07 -0300

    Implement the `LocalGalleryImage` DTO to achieve the same goal: reduce refereces to `GalleryImage`

--- NASAGallery/NASAGallery/Gallery Cache/LocalGalleryImage.swift
+++ NASAGallery/NASAGallery/Gallery Cache/LocalGalleryImage.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+// Internal type!
+public struct LocalGalleryImage: Equatable {
+    let title: String
+    let url: URL
+    let date: String
+    let explanation: String
+    let mediaType: String
+    
+    let copyright: String?
+    let hdurl: URL?
+    let thumbnailUrl: URL?
+    
+    public init(title: String, url: URL, date: String, explanation: String, mediaType: String, copyright: String?, hdurl: URL?, thumbnailUrl: URL?) {
+        self.title = title
+        self.url = url
+        self.date = date
+        self.explanation = explanation
+        self.mediaType = mediaType
+        self.copyright = copyright
+        self.hdurl = hdurl
+        self.thumbnailUrl = thumbnailUrl
+    }
+}
