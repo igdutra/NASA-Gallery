628b8187020dd66f1c67b849459aa9d65666cfd7
Ivo Dutra <ivogdutra@gmail.com>
2024-03-13 20:04:22 -0300

    Fix API End to end tests

--- NASAGallery/NASAGalleryAPIEndToEndTests/NASAGalleryAPIEndToEndTests.swift
+++ NASAGallery/NASAGalleryAPIEndToEndTests/NASAGalleryAPIEndToEndTests.swift
@@ -50,17 +50,17 @@ private extension NASAGalleryEndToEndTests {
         return loader
     }
     
-    func assert(_ item1: GalleryItem, matches item2: GalleryItem, file: StaticString = #file, line: UInt = #line) {
-        XCTAssertEqual(item1.title, item2.title, "Expected title to match", file: file, line: line)
-        XCTAssertEqual(item1.url, item2.url, "Expected URL to match", file: file, line: line)
-        XCTAssertEqual(item1.date, item2.date, "Expected date to match", file: file, line: line)
-        XCTAssertEqual(item1.explanation, item2.explanation, "Expected explanation to match", file: file, line: line)
-        XCTAssertEqual(item1.mediaType, item2.mediaType, "Expected mediaType to match", file: file, line: line)
+    func assert(_ image1: GalleryImage, matches image2: GalleryImage, file: StaticString = #file, line: UInt = #line) {
+        XCTAssertEqual(image1.title, image2.title, "Expected title to match", file: file, line: line)
+        XCTAssertEqual(image1.url, image2.url, "Expected URL to match", file: file, line: line)
+        XCTAssertEqual(image1.date, image2.date, "Expected date to match", file: file, line: line)
+        XCTAssertEqual(image1.explanation, image2.explanation, "Expected explanation to match", file: file, line: line)
+        XCTAssertEqual(image1.mediaType, image2.mediaType, "Expected mediaType to match", file: file, line: line)
         
         // Optional fields
-        XCTAssertEqual(item1.copyright, item2.copyright, "Expected copyright to match", file: file, line: line)
-        XCTAssertEqual(item1.hdurl, item2.hdurl, "Expected HD URL to match", file: file, line: line)
-        XCTAssertEqual(item1.thumbnailUrl, item2.thumbnailUrl, "Expected thumbnail URL to match", file: file, line: line)
+        XCTAssertEqual(image1.copyright, image2.copyright, "Expected copyright to match", file: file, line: line)
+        XCTAssertEqual(image1.hdurl, image2.hdurl, "Expected HD URL to match", file: file, line: line)
+        XCTAssertEqual(image1.thumbnailUrl, image2.thumbnailUrl, "Expected thumbnail URL to match", file: file, line: line)
     }
     
     // TODO: Create new form to abstract that
@@ -82,8 +82,8 @@ private extension NASAGalleryEndToEndTests {
         return components.url!
     }
     
-    func expectedItemOn10Dec2023() -> GalleryItem {
-        GalleryItem(title: "Big Dipper over Pyramid Mountain",
+    func expectedItemOn10Dec2023() -> GalleryImage {
+        GalleryImage(title: "Big Dipper over Pyramid Mountain",
                     url: URL(string: "https://apod.nasa.gov/apod/image/2312/BigDipperMt2_Cullen_960.jpg")!,
                     date: "2023-12-10",
                     explanation: "When did you first learn to identify this group of stars? Although they are familiar to many people around the world, different cultures have associated this asterism with different icons and folklore. Known in the USA as the Big Dipper, the stars are part of a constellation designated by the International Astronomical Union in 1922 as the Great Bear (Ursa Major).  The recognized star names of these stars are (left to right) Alkaid, Mizar/Alcor, Alioth, Megrez, Phecda, Merak, and Dubhe.  Of course, stars in any given constellation are unlikely to be physically related. But surprisingly, most of the Big Dipper stars do seem to be headed in the same direction as they plough through space, a property they share with other stars spread out over an even larger area across the sky.  Their measured common motion suggests that they all belong to a loose, nearby star cluster, thought to be on average only about 75 light-years away and up to 30 light-years across. The cluster is more properly known as the Ursa Major Moving Group. The featured image captured the iconic stars in 2017 above Pyramid Mountain in Alberta, Canada.   Night Sky Network webinar: APOD editor to review coolest space images of 2023",
@@ -93,8 +93,8 @@ private extension NASAGalleryEndToEndTests {
                     thumbnailUrl: nil)
     }
 
-    func expectedItemOn11Dec2023() -> GalleryItem {
-        return GalleryItem(
+    func expectedItemOn11Dec2023() -> GalleryImage {
+        return GalleryImage(
             title: "Solar Minimum versus Solar Maximum",
             url: URL(string: "https://www.youtube.com/embed/JqH0diwqcUM?rel=0")!,
             date: "2023-12-11",
