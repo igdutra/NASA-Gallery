d1707d048a5c20ddd584996e325707c0aa04658c
Ivo Dutra <ivogdutra@gmail.com>
2024-03-22 17:55:34 -0300

    update ReadMe with Load gallery from cache sad paths

--- NASAGallery/NASAGalleryTests/Gallery Cache/LoadGalleryFromCacheUseCaseTests.swift
+++ NASAGallery/NASAGalleryTests/Gallery Cache/LoadGalleryFromCacheUseCaseTests.swift
@@ -17,13 +17,14 @@ import NASAGallery
 
  #### Primary course:
  1. Execute "Retrieve Cached APOD Gallery" command.
- 2. System fetches APOD Gallery data from cache.
+ 2. System retrieves APOD Gallery data from cache.
  3. System validates cache age againts maximum age: verify if it is less than 2 days old.
  4. System creates APOD Gallery from valid cached data.
  5. System delivers APOD Gallery.
 
- #### Error course (sad path):
- 1. System delivers no APOD Gallery.
+ #### Retrieval error course (sad path):
+ 1. System deletes cache.
+ 2. System delivers error.
 
  #### Expired cache course (sad path):
  1. System deletes cache.
