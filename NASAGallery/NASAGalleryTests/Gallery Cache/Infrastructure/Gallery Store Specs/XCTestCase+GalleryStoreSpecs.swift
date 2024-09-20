28f993f4ef3ad07643b6be10b68fff63c616321e
Ivo Dutra <ivogdutra@gmail.com>
2024-09-20 08:31:34 -0300

    Add helper methods to `XCTestCase` so we can beter reuse the do/catch blocks

---
+++ NASAGallery/NASAGalleryTests/Gallery Cache/Infrastructure/Gallery Store Specs/XCTestCase+GalleryStoreSpecs.swift
@@ -0,0 +1,13 @@
+//
+//  XCTestCase+GalleryStoreSpecs.swift
+//  NASAGalleryTests
+//
+//  Created by Ivo on 20/09/24.
+//
+
+import XCTest
+import NASAGallery
+
+extension GalleryStoreSpecs where Self: XCTestCase {
+    
+}
---
+++ NASAGallery/NASAGalleryTests/Shared Test Helpers/XCTestCase+Throws.swift
@@ -0,0 +1,69 @@
+//
+//  XCTestCase+Throws.swift
+//  NASAGalleryTests
+//
+//  Created by Ivo on 20/09/24.
+//
+
+import XCTest
+
+extension XCTestCase {
+    
+    /// Example usage:
+    /// ```
+    /// func test_retrieve_onEmptyCache_deliversEmpty() async throws {
+    ///     let sut = makeSUT()
+    ///     let result = await expectNoThrowAsync(try await sut.retrieve())
+    ///     XCTAssertNil(result, "Expected nil, got \(String(describing: result))")
+    /// }
+    /// ```
+    func expectNoThrowAsync<AnyResult>(_ expression: @autoclosure () async throws -> AnyResult,
+                                       _ message: @autoclosure () -> String = "",
+                                       file: StaticString = #file, line: UInt = #line
+    ) async -> AnyResult? {
+        do {
+            return try await expression()
+        } catch {
+            XCTFail(message().isEmpty ? "Expected no error, but got: \(error)" : message(), file: file, line: line)
+            return nil
+        }
+    }
+    
+    /// Example usage when expecting a specific error:
+    /// ```swift
+    /// func test_retrieve_onInvalidCache_throwsSpecificError() async {
+    ///     let sut = makeSUT()
+    ///     let error = await expectThrowAsync(try await sut.retrieve(), YourExpectedErrorType.self)
+    ///     XCTAssertEqual(error as? YourExpectedErrorType, YourExpectedErrorType.someCase)
+    /// }
+    /// ```
+    /// Example usage when you don't care about the error type:
+    /// ```swift
+    /// func test_retrieve_onInvalidCache_throwsAnyError() async {
+    ///     let sut = makeSUT()
+    ///     let error = await expectThrowAsync(try await sut.retrieve())
+    ///     XCTAssertNotNil(error, "Expected an error, but no error was thrown")
+    /// }
+    /// ```
+    @discardableResult
+    func expectThrowAsync<AnyResult, ExpectedError: Error>(_ expression: @autoclosure () async throws -> AnyResult,
+                                                           _ expectedError: ExpectedError.Type? = nil,
+                                                           _ message: @autoclosure () -> String = "",
+                                                           file: StaticString = #file, line: UInt = #line
+    ) async -> Error? {
+        do {
+            _ = try await expression()
+            XCTFail(message().isEmpty ? "Expected an error, but no error was thrown" : message(), file: file, line: line)
+            return nil
+        } catch let error as ExpectedError where expectedError != nil {
+            return error
+        } catch {
+            if expectedError == nil {
+                return error
+            } else {
+                XCTFail(message().isEmpty ? "Expected error of type \(String(describing: expectedError)), but got: \(error)" : message(), file: file, line: line)
+                return nil
+            }
+        }
+    }
+}
