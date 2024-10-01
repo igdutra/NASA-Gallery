ca9d6ed8cd278a892ef227f5d4cd659de0533bb8
Ivo Dutra <ivogdutra@gmail.com>
2024-10-01 19:39:17 -0300

    Add Empty `CoreDataGalleryStore`

--- NASAGallery/NASAGalleryTests/Gallery Cache/Infrastructure/CoreDataGalleryStore.swift
+++ NASAGallery/NASAGalleryTests/Gallery Cache/Infrastructure/CoreDataGalleryStore.swift
@@ -7,9 +7,73 @@
 
 import XCTest
 
-final class CoreDataGalleryStore: XCTestCase {
-
-    func test() {
-        XCTFail("Initial failing test.")
+final class CoreDataGalleryStore: XCTestCase, FailableGalleryStoreSpecs {
+    
+    func test_retrieve_onRetrivalError_fails() async throws {
+    
+    }
+    
+    func test_retrieve_onRetrivalError_hasNoSideEffects() async throws {
+    
+    }
+    
+    func test_insert_onInsertionError_fails() async throws {
+        
+    }
+    
+    func test_insert_onInsertionError_hasNoSideEffects() async throws {
+        
+    }
+    
+    func test_delete_onDeletionError_fails() async throws {
+        
+    }
+    
+    func test_delete_onDeletionError_hasNoSideEffects() async throws {
+        
+    }
+    
+    func test_retrieve_onEmptyCache_deliversEmpty() async throws {
+        
+    }
+    
+    func test_retrieve_onNonEmptyCache_succeedsWithCache() async throws {
+        
+    }
+    
+    func test_retrieve_onEmptyCache_hasNoSideEffects() async throws {
+        
+    }
+    
+    func test_retrieve_onNonEmptyCache_hasNoSideEffects() async throws {
+        
+    }
+    
+    func test_insert_onEmptyCache_succeedsWithNoThrow() async throws {
+        
+    }
+    
+    func test_insert_onNonEmptyCache_succeedsWithNoThrow() async throws {
+        
+    }
+    
+    func test_insert_onNonEmptyCache_succeedsWithOverridingPreviousCache() async throws {
+        
+    }
+    
+    func test_delete_onEmptyCache_succeeds() async throws {
+        
+    }
+    
+    func test_delete_onNonEmptyCache_succeeds() async throws {
+        
+    }
+    
+    func test_delete_onEmptyCache_hasNoSideEffects() async throws {
+        
+    }
+    
+    func test_delete_onNonEmptyCache_hasNoSideEffects() async throws {
+        
     }
 }
