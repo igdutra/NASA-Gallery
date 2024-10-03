e2b7cb272cc9a04ecc530a4251f3ceb574278e67
Ivo Dutra <ivogdutra@gmail.com>
2024-10-03 08:13:39 -0300

    Reorganize testes on `CoreDataGalleryStore`

--- NASAGallery/NASAGalleryTests/Gallery Cache/Infrastructure/CoreDataGalleryStore.swift
+++ NASAGallery/NASAGalleryTests/Gallery Cache/Infrastructure/CoreDataGalleryStore.swift
@@ -9,35 +9,10 @@ import XCTest
 
 final class CoreDataGalleryStore: XCTestCase, FailableGalleryStoreSpecs {
     
-    func test_retrieve_onRetrivalError_fails() async throws {
+    // MARK: - Retrieve
     
-    }
-    
-    func test_retrieve_onRetrivalError_hasNoSideEffects() async throws {
-    
-    }
-    
-    func test_insert_onInsertionError_fails() async throws {
-        
-    }
-    
-    func test_insert_onInsertionError_hasNoSideEffects() async throws {
-        
-    }
-    
-    func test_delete_onDeletionError_fails() async throws {
-        
-    }
-    
-    func test_delete_onDeletionError_hasNoSideEffects() async throws {
-        
-    }
-    
+
     func test_retrieve_onEmptyCache_deliversEmpty() async throws {
-        
-    }
-    
-    func test_retrieve_onNonEmptyCache_succeedsWithCache() async throws {
         
     }
     
@@ -48,6 +23,20 @@ final class CoreDataGalleryStore: XCTestCase, FailableGalleryStoreSpecs {
     func test_retrieve_onNonEmptyCache_hasNoSideEffects() async throws {
         
     }
+    
+    func test_retrieve_onNonEmptyCache_succeedsWithCache() async throws {
+        
+    }
+    
+    func test_retrieve_onRetrivalError_fails() async throws {
+    
+    }
+    
+    func test_retrieve_onRetrivalError_hasNoSideEffects() async throws {
+    
+    }
+    
+    // MARK: - Insert
     
     func test_insert_onEmptyCache_succeedsWithNoThrow() async throws {
         
@@ -61,11 +50,17 @@ final class CoreDataGalleryStore: XCTestCase, FailableGalleryStoreSpecs {
         
     }
     
-    func test_delete_onEmptyCache_succeeds() async throws {
+    func test_insert_onInsertionError_fails() async throws {
         
     }
     
-    func test_delete_onNonEmptyCache_succeeds() async throws {
+    func test_insert_onInsertionError_hasNoSideEffects() async throws {
+        
+    }
+    
+    // MARK: - Delete
+
+    func test_delete_onEmptyCache_succeeds() async throws {
         
     }
     
@@ -73,7 +68,19 @@ final class CoreDataGalleryStore: XCTestCase, FailableGalleryStoreSpecs {
         
     }
     
+    func test_delete_onNonEmptyCache_succeeds() async throws {
+        
+    }
+    
     func test_delete_onNonEmptyCache_hasNoSideEffects() async throws {
+        
+    }
+    
+    func test_delete_onDeletionError_fails() async throws {
+        
+    }
+    
+    func test_delete_onDeletionError_hasNoSideEffects() async throws {
         
     }
 }
