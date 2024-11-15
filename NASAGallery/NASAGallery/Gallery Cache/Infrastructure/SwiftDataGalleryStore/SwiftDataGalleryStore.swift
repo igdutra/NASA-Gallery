c842748a3ebdc6ae15a2ce2f2e10fa9c99d65b1a
Ivo Dutra <ivogdutra@gmail.com>
2024-11-14 21:39:47 -0300

    Add first files

---
+++ NASAGallery/NASAGallery/Gallery Cache/Infrastructure/SwiftDataGalleryStore/SwiftDataGalleryStore.swift
@@ -0,0 +1,10 @@
+//
+//  SwiftDataGalleryStore.swift
+//  NASAGallery
+//
+//  Created by Ivo on 14/11/24.
+//
+
+import Foundation
+import SwiftData
+
---
+++ NASAGallery/NASAGalleryTests/Gallery Cache/Infrastructure/SwiftDataGalleryStoreTests.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftDataGalleryStoreTests.swift
+//  NASAGalleryTests
+//
+//  Created by Ivo on 14/11/24.
+//
+
+import XCTest
+
+final class SwiftDataGalleryStoreTests: XCTestCase {
+
+    func test() {
+        XCTFail("Initial failing test.")
+    }
+}
