61d7e06d9ca68895da4cb1870a770f1afc89c099
Ivo Dutra <ivogdutra@gmail.com>
2024-11-20 10:06:08 -0300

    Add both `SwiftDataStoredGalleryImage` and `SwiftDataStoredGalleryCache` models. They are all that it takes to setup swiftdata (using in main thread). Actor to be defined

--- NASAGallery/NASAGallery/Gallery Cache/Infrastructure/SwiftDataGalleryStore/Swift Data Models/SwiftDataStoredGalleryImage.swift
+++ NASAGallery/NASAGallery/Gallery Cache/Infrastructure/SwiftDataGalleryStore/Swift Data Models/SwiftDataStoredGalleryImage.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+final class SwiftDataStoredGalleryImage {
+    var title: String
+    @Attribute(.unique) var url: URL
+    var date: Date
+    var explanation: String
+    var mediaType: String
+
+    var copyright: String?
+    var hdurl: URL?
+    var thumbnailUrl: URL?
+
+    var imageData: Data?
+
+    var cache: SwiftDataStoredGalleryCache
+    
+    init(title: String,
+         url: URL,
+         date: Date,
+         explanation: String,
+         mediaType: String,
+         copyright: String? = nil,
+         hdurl: URL? = nil,
+         thumbnailUrl: URL? = nil,
+         imageData: Data? = nil,
+         cache: SwiftDataStoredGalleryCache) {
+        self.title = title
+        self.url = url
+        self.date = date
+        self.explanation = explanation
+        self.mediaType = mediaType
+        self.copyright = copyright
+        self.hdurl = hdurl
+        self.thumbnailUrl = thumbnailUrl
+        self.imageData = imageData
+        self.cache = cache
+    }
+}
