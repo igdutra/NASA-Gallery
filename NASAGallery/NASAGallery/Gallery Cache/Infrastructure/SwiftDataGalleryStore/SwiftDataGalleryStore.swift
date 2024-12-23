1f29bf6345ddb72d07bdb55fd5a06bd7e7806f20
Ivo Dutra <ivogdutra@gmail.com>
2024-12-23 07:46:35 -0300

    Make sure that `SwiftData` implementation of the infrastructure will also rollback context in case of any errors

--- NASAGallery/NASAGallery/Gallery Cache/Infrastructure/SwiftDataGalleryStore/SwiftDataGalleryStore.swift
+++ NASAGallery/NASAGallery/Gallery Cache/Infrastructure/SwiftDataGalleryStore/SwiftDataGalleryStore.swift
@@ -11,52 +11,67 @@ import SwiftData
 @ModelActor
 public final actor SwiftDataGalleryStore: GalleryStore {
     public func retrieve() async throws -> LocalGalleryCache? {
-        let fetchDescriptor = FetchDescriptor<SwiftDataStoredGalleryCache>()
-        guard let storedCache = try modelContext.fetch(fetchDescriptor).first else {
-            return nil
-        }
-        let localGallery = storedCache.gallery
-            .sorted { $0.sortIndex < $1.sortIndex }
-            .map {
-                LocalGalleryImage(title: $0.title, url: $0.url, date: $0.date, explanation: $0.explanation, mediaType: $0.mediaType, copyright: $0.copyright, hdurl: $0.hdurl, thumbnailUrl: $0.thumbnailUrl)
+        do {
+            let fetchDescriptor = FetchDescriptor<SwiftDataStoredGalleryCache>()
+            guard let storedCache = try modelContext.fetch(fetchDescriptor).first else {
+                return nil
             }
-        
-        return LocalGalleryCache(gallery: localGallery, timestamp: storedCache.timestamp)
+            let localGallery = storedCache.gallery
+                .sorted { $0.sortIndex < $1.sortIndex }
+                .map {
+                    LocalGalleryImage(title: $0.title, url: $0.url, date: $0.date, explanation: $0.explanation, mediaType: $0.mediaType, copyright: $0.copyright, hdurl: $0.hdurl, thumbnailUrl: $0.thumbnailUrl)
+                }
+            
+            return LocalGalleryCache(gallery: localGallery, timestamp: storedCache.timestamp)
+        } catch {
+            modelContext.rollback()
+            throw error
+        }
     }
     
     public func insert(_ cache: LocalGalleryCache) async throws {
-        // Note: make sure to override previous cache
-        try await delete()
-        
-        let storedCache = SwiftDataStoredGalleryCache(timestamp: cache.timestamp, gallery: [])
-        
-        let storedGallery = cache.gallery.enumerated().map { (index, image) in
-            SwiftDataStoredGalleryImage(sortIndex: index,
-                                        title: image.title,
-                                        url: image.url,
-                                        date: image.date,
-                                        explanation: image.explanation,
-                                        mediaType: image.mediaType,
-                                        copyright: image.copyright,
-                                        hdurl: image.hdurl,
-                                        thumbnailUrl: image.thumbnailUrl,
-                                        imageData: nil,
-                                        cache: storedCache)
+        do {
+            // Note: make sure to override previous cache
+            try await delete()
+            
+            let storedCache = SwiftDataStoredGalleryCache(timestamp: cache.timestamp, gallery: [])
+            
+            let storedGallery = cache.gallery.enumerated().map { (index, image) in
+                SwiftDataStoredGalleryImage(sortIndex: index,
+                                            title: image.title,
+                                            url: image.url,
+                                            date: image.date,
+                                            explanation: image.explanation,
+                                            mediaType: image.mediaType,
+                                            copyright: image.copyright,
+                                            hdurl: image.hdurl,
+                                            thumbnailUrl: image.thumbnailUrl,
+                                            imageData: nil,
+                                            cache: storedCache)
+            }
+            
+            storedCache.gallery = storedGallery
+            
+            modelContext.insert(storedCache)
+            try modelContext.save()
+        } catch {
+            modelContext.rollback()
+            throw error
         }
-        
-        storedCache.gallery = storedGallery
-        
-        modelContext.insert(storedCache)
-        try modelContext.save()
     }
     
     public func delete() async throws {
-        let fetchDescriptor = FetchDescriptor<SwiftDataStoredGalleryCache>()
-        let allCaches = try modelContext.fetch(fetchDescriptor)
-        
-        allCaches.forEach { modelContext.delete($0) }
-        
-        try modelContext.save()
+        do {
+            let fetchDescriptor = FetchDescriptor<SwiftDataStoredGalleryCache>()
+            let allCaches = try modelContext.fetch(fetchDescriptor)
+            
+            allCaches.forEach { modelContext.delete($0) }
+            
+            try modelContext.save()
+        } catch {
+            modelContext.rollback()
+            throw error
+        }
     }
 }
 
