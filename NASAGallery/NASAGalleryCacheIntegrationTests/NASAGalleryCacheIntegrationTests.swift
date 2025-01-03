70df7dc530675c75a98964c451587a198507fa95
Ivo Dutra <ivogdutra@gmail.com>
2025-01-03 09:27:52 -0300

    Add `NASAGalleryCacheIntegrationTests` as a separate TestPlan, and make sure to include it in the CI test plan and exclude in the default test plan (to be ran multiple times)

---
+++ NASAGallery/NASAGalleryCacheIntegrationTests/NASAGalleryCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  NASAGalleryCacheIntegrationTests.swift
+//  NASAGalleryTests
+//
+//  Created by Ivo on 03/01/25.
+//
+
+import XCTest
+
+final class NASAGalleryCacheIntegrationTests: XCTestCase {
+
+    func test() {
+    }
+}
