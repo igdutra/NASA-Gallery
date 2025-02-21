89e37c1767f101e1b90d5fab35b1e5791ce2579d
Ivo Dutra <ivogdutra@gmail.com>
2025-02-21 10:08:44 -0300

    Create prototype project

---
+++ Prototype/Prototype/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Prototype
+//
+//  Created by Ivo on 21/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        EmptyView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Prototype/Prototype/PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeApp.swift
+//  Prototype
+//
+//  Created by Ivo on 21/02/25.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
