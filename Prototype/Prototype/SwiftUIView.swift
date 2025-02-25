f1c67e824dba33c8eeb1eb05864fcdf80fa00239
Ivo Dutra <ivogdutra@gmail.com>
2025-02-25 09:35:12 -0300

    Add 2 different basic views

---
+++ Prototype/Prototype/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Prototype
+//
+//  Created by Ivo on 25/02/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
