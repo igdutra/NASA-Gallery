a71344e919682abb4008d9a817cffc8063aee9e3
Ivo Dutra <ivogdutra@gmail.com>
2025-02-26 08:06:48 -0300

    Add simple list viwere

--- Prototype/Prototype/SwiftUIView.swift
+++ Prototype/Prototype/SwiftUIView.swift
@@ -10,19 +10,19 @@ import SwiftUI
 // Ref:     https://stackoverflow.com/questions/69489035/how-to-get-a-grid-in-swiftui-with-custom-layout-different-cell-size
 
 struct SwiftUIView: View {
-    let colors: [Color] = [
-        .red,
-        .green,
-        .blue
-    ]
-    
     var body: some View {
         List {
-            ForEach(colors, id: \.self) { color in
-                color
-                    .frame(maxWidth: .infinity, minHeight: 100)
+            ForEach(apodImages, id: \.self) { resource in
+                Image(resource)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: .infinity, maxHeight: 400)
+                    .listRowBackground(Color.black)
+                    .listRowSeparator(.hidden)
             }
         }
+        .listStyle(.plain)
+        .ignoresSafeArea()
     }
 }
 
