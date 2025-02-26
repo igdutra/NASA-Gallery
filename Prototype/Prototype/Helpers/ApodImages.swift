42789453ab94e515f6301abf6d5af12800ffc948
Ivo Dutra <ivogdutra@gmail.com>
2025-02-26 08:03:26 -0300

    Add image reosurce helper

---
+++ Prototype/Prototype/Helpers/ApodImages.swift
@@ -0,0 +1,15 @@
+//
+//  ApodImages.swift
+//  Prototype
+//
+//  Created by Ivo on 26/02/25.
+//
+
+import DeveloperToolsSupport
+import Foundation
+
+let apodImages: [ImageResource] = [
+    .apod1, .apod2, .apod3, .apod4, .apod5,
+    .apod6, .apod7, .apod8, .apod9, .apod10,
+    .apod11, .apod12, .apod13, .apod14, .apod15
+]
