5baec6816dccf38494de306c196adeb461e96c92
Ivo Dutra <ivogdutra@gmail.com>
2025-03-13 19:08:51 -0300

    Organize Rows: finally move on with 3 only types of rows

--- Prototype/Prototype/SwiftUIView.swift
+++ Prototype/Prototype/SwiftUIView.swift
@@ -8,7 +8,6 @@
 // Reply this with a better solution:    https://stackoverflow.com/questions/69489035/how-to-get-a-grid-in-swiftui-with-custom-layout-different-cell-size
 
 // TODO: fade in
-// TODO: extract Image helpers -> use scale to fit only on big images, smaller ones are fill
 
 import SwiftUI
 
@@ -18,12 +17,11 @@ struct SwiftUIView: View {
         GeometryReader { geometry in
             ScrollView {
                 VStack {
-                    row5(size: geometry.size)
-//                    row4(totalHeight: geometry.size.height)
-//                    row1()
-//                    row3(totalHeight: geometry.size.height * (1.4)) // Use 2/3 of available space
-//                    row1()
-//                    row2(totalHeight: geometry.size.height * (2/3)) // Use 2/3 of available space
+//                    row1(with: .apod1)
+//                    row5(size: geometry.size)
+//                    row1(with: .apod15)
+                    
+                    columnRow(size: geometry.size)
                 }
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -33,96 +31,53 @@ struct SwiftUIView: View {
     
     // MARK: - Row Types
     
-    func row1() -> some View {
-        fillImage(for: .apod1)
+    func bigRow(with image: ImageResource) -> some View {
+        fillImage(for: image)
     }
     
-    func row2(totalHeight: CGFloat) -> some View {
-        HStack {
-            VStack {
-                resizedImage(for: .apod2)
-                    .frame(maxHeight: totalHeight / 2)
+    func columnRow(size: CGSize) -> some View {
+        let totalHeight = size.height
+        let firstColumnWidth = size.width / 3
+        let goldenRatio: CGFloat = 1.618
+        let height1 = totalHeight / (1 + goldenRatio)
+        let height2 = totalHeight - height1
 
-                resizedImage(for: .apod3)
-                    .frame(maxHeight: totalHeight / 2)
-                
-                resizedImage(for: .apod6)
-                    .frame(maxHeight: totalHeight / 2)
-            }
+        return HStack(spacing: 100) {
             VStack {
-                let goldenRatio: CGFloat = 1.618
-                let height1 = totalHeight / (1 + goldenRatio) // Right column: golden ratio
-                let height2 = totalHeight - height1
-                
-                resizedImage(for: .apod4)
-                    .frame(maxHeight: height1)
-                resizedImage(for: .apod5)
+                fillImage(for: .apod2)
                     .clipped()
-                    .frame(maxHeight: height2)
+                
+                fillImage(for: .apod3)
+                    .clipped()
+                
+                fillImage(for: .apod6)
+                    .clipped()
+                fillImage(for: .apod8)
+                    .clipped()
+                fillImage(for: .apod9)
+                    .clipped()
             }
-
+            .frame(width: firstColumnWidth)
+            .debugBorder()
+            
+            VStack {
+                fillImage(for: .apod4)
+                    .smoothEdges()
+                    .debugBorder()
+                
+                fillImage(for: .apod12)
+                    .smoothEdges()
+                    .debugBorder()
+                fillImage(for: .apod13)
+                    .smoothEdges()
+                    .debugBorder()
+            }
+            .frame(width: firstColumnWidth * 2)
         }
         .frame(height: totalHeight)
     }
     
-    func row3(totalHeight: CGFloat) -> some View {
-        HStack {
-            VStack {
-                resizedImage(for: .apod10)
-                    .frame(maxHeight: totalHeight / 4)
-
-                resizedImage(for: .apod11)
-                    .frame(maxHeight: totalHeight / 4)
-                
-                resizedImage(for: .apod12)
-                    .frame(maxHeight: totalHeight / 4)
-                
-                resizedImage(for: .apod1)
-                    .frame(maxHeight: totalHeight / 4)
-            }
-            .padding(.bottom, 100)
-
-            VStack {
-                resizedImage(for: .apod13)
-                    .frame(maxHeight: totalHeight / 4)
-
-                resizedImage(for: .apod14)
-                    .frame(maxHeight: totalHeight / 4)
-                
-                resizedImage(for: .apod15)
-                    .frame(maxHeight: totalHeight / 4)
-                
-                resizedImage(for: .apod11)
-                    .frame(maxHeight: totalHeight / 4)
-            }
-            .padding(.top, 100)
-
-        }
-        .frame(height: totalHeight)
-    }
-    
-    func row4(totalHeight: CGFloat) -> some View {
-        HStack {
-            VStack {
-                resizedImage(for: .apod1)
-                    .frame(maxHeight: totalHeight / 4)
-
-                resizedImage(for: .apod2)
-                resizedImage(for: .apod6)
-            }
-            VStack {
-                resizedImage(for: .apod5)
-                resizedImage(for: .apod6)
-                resizedImage(for: .apod7)
-                resizedImage(for: .apod8)
-                resizedImage(for: .apod9)
-
-            }
-        }
-        .frame(height: totalHeight)
-    }
-    
-    func row5(size: CGSize) -> some View {
+    func horizontalRow(size: CGSize) -> some View {
         let height = size.height / 3
         let fifth = size.width / 5
         
@@ -155,12 +110,12 @@ struct SwiftUIView: View {
                     .smoothEdges()
             }
             .frame(maxHeight: height)
-
+            
             HStack {
                 fitImage(for: .apod5)
                     .clipShape(RoundedRectangle(cornerRadius: 8))
                     .frame(maxWidth: fifth * 4)
-
+                
                 fillImage(for: .apod7)
                     .containerRelativeFrame(.horizontal) { size, axis in
                         size * 0.2
