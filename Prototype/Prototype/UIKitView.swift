0f07d4ca4718a5130438c4457124f9ec40846487
Ivo Dutra <ivogdutra@gmail.com>
2025-03-17 19:46:53 -0300

    Create a basic UIcollectionViewController, that will display  createBigRowLayout

--- Prototype/Prototype/UIKitView.swift
+++ Prototype/Prototype/UIKitView.swift
@@ -5,14 +5,106 @@
 //  Created by Ivo on 25/02/25.
 //
 
-import SwiftUI
+import UIKit
+import DeveloperToolsSupport
 
-struct UIKitView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+// MARK: - CELL
+
+final class APODImageCell: UICollectionViewCell {
+    
+    static let reuseIdentifier = "APODImageCell"
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 8
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+    
+    required init?(coder: NSCoder) { nil }
+    
+    func configure(with image: ImageResource) {
+        imageView.image = UIImage(resource: .apod1)
     }
 }
 
+// MARK: - CONTROLLER
+
+final class APODGalleryViewController: UIViewController {
+    
+    private var collectionView: UICollectionView!
+    private var dataSource: UICollectionViewDiffableDataSource<Int, ImageResource>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        
+        setupCollectionView()
+        setupDataSource()
+        applySnapshot()
+    }
+    
+    private func setupCollectionView() {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, environment in
+            return self.createBigRowLayout()
+        }
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .black
+        collectionView.register(APODImageCell.self, forCellWithReuseIdentifier: APODImageCell.reuseIdentifier)
+        
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    private func setupDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Int, ImageResource>(collectionView: collectionView) { (collectionView, indexPath, imageResource) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: APODImageCell.reuseIdentifier, for: indexPath) as! APODImageCell
+            cell.configure(with: imageResource)
+            return cell
+        }
+    }
+    
+    private func applySnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Int, ImageResource>()
+        snapshot.appendSections([0])
+        snapshot.appendItems([.apod1]) // Using apod1 as the image
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+    
+    private func createBigRowLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.5))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+}
+
+// MARK: - Preview
+
 #Preview {
-    UIKitView()
+    APODGalleryViewController()
 }
