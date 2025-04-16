587c0384160e29da2386cf40ef20f9f2726e68e7
Ivo Dutra <ivogdutra@gmail.com>
2025-04-16 06:56:29 -0300

    Failing test: inject async context inside XCTest and ViewDidLoad

--- NASAGallery/NASAGalleryiOSTests/GalleryViewControllerTests.swift
+++ NASAGallery/NASAGalleryiOSTests/GalleryViewControllerTests.swift
@@ -6,21 +6,41 @@
 //
 
 import XCTest
+import UIKit
 import NASAGallery
 
-final class GalleryViewController {
-    init(loader: GalleryLoader) {
+final class GalleryViewController: UIViewController {
+    private var loader: GalleryLoader?
+    
+    convenience init(loader: GalleryLoader) {
+        self.init()
+        self.loader = loader
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        Task {
+            try await loader?.load()
+        }
     }
 }
 
 final class GalleryViewControllerTests: XCTestCase {
-
     func test_init_doesNotLoadGallery() {
         let loader = GalleryLoaderSpy()
         _ = GalleryViewController(loader: loader)
         
         XCTAssertEqual(loader.loadCallCount, 0)
+    }
+    
+    func failingTest_test_viewDidLoad_loadGallery() {
+        let loader = GalleryLoaderSpy()
+        let sut = GalleryViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
     }
 }
 
