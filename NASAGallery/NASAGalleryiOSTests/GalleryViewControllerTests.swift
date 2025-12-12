a1139a202c04ca4f965d160b9d4be2f3ee2d4422
Ivo Dutra <ivogdutra@gmail.com>
2025-12-11 22:22:05 -0300

    Add goals todo list

--- NASAGallery/NASAGalleryiOSTests/GalleryViewControllerTests.swift
+++ NASAGallery/NASAGalleryiOSTests/GalleryViewControllerTests.swift
@@ -10,6 +10,25 @@ import NASAGallery
 import NASAGalleryiOS
 import UIKit
 
+
+/*
+ > Load feed automatically when view is presented
+
+ > Allow customer to manually reload feed (pull to refresh)
+
+ > Show a loading indicator while loading feed
+
+ > Render all loaded feed items (location, image, description)
+
+ >>  Image loading experience
+    - Load when image view is visible (on screen)
+    - Cancel when image view is out of screen
+    - Show a loading indicator while loading image (shimmer)
+    - Option to retry on image download error
+    - Preload when image view is near visibleU
+ 
+ */
+
 @MainActor
 @Suite(.timeLimit(.minutes(1)))
 struct GalleryViewControllerTests {
