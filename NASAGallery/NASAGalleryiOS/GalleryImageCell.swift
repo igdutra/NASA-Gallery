afc9561763a1289a87db1d6ff8c8cde8f085c5f4
Ivo Dutra <ivogdutra@gmail.com>
2025-12-30 13:41:24 -0300

    Add activity indicator to GalleryImageCell

--- NASAGallery/NASAGalleryiOS/GalleryImageCell.swift
+++ NASAGallery/NASAGalleryiOS/GalleryImageCell.swift
@@ -9,19 +9,65 @@ import Foundation
 import NASAGallery
 import UIKit
 
-public final class GalleryImageCell: UICollectionViewCell {
+@MainActor public final class GalleryImageCell: UICollectionViewCell {
+    // MARK: - Components
+    
+    private let activityIndicator = UIActivityIndicatorView(style: .medium)
+    
+    // MARK: - Init & lifecycle
+    
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupActivityIndicator()
+    }
+    
+    required init?(coder: NSCoder) { nil }
+    
+    public override func prepareForReuse() {
+        super.prepareForReuse()
+        stopLoading()
+        self.contentConfiguration = nil
+    }
+    
+    // MARK: - Methods
+    
+    private func setupActivityIndicator() {
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(activityIndicator)
+        
+        NSLayoutConstraint.activate([
+            activityIndicator.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ])
+        
+        bringSpinnerToFront() // Note: most likely not needed
+    }
+    
+    private func bringSpinnerToFront() {
+        contentView.bringSubviewToFront(activityIndicator)
+    }
+    
+    public var isLoading: Bool {
+        activityIndicator.isAnimating
+    }
+    
+    public func startLoading() {
+        activityIndicator.startAnimating()
+        bringSpinnerToFront()
+    }
+    
+    public func stopLoading() {
+        activityIndicator.stopAnimating()
+    }
+    
     // Configure content using UIListContentConfiguration to keep the same subtitle style
     func apply(model: GalleryImage) {
         var content = UIListContentConfiguration.subtitleCell()
         content.text = model.title
         content.secondaryText = model.date.formatted(date: .abbreviated, time: .omitted)
         self.contentConfiguration = content
-    }
 
-    // Note: this will be used later on as TDD flow
-//    public override func prepareForReuse() {
-//        super.prepareForReuse()
-//        // Reset any previous content/accessories
-//        self.contentConfiguration = nil
-//    }
+    }
 }
